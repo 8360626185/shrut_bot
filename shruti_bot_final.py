33488e1ba1de76d3e6776d601602147d47879a27
8360626185 <rajkumarjayswal1@gmail.com>
2026-03-11 13:18:50 +0530

    Update shruti_bot_final.py

--- shruti_bot_final.py
+++ shruti_bot_final.py
@@ -14,8 +14,8 @@ from telegram.ext import (
 )
 
 # ════════════════════════════════════════════════════════════════
-BOT_TOKEN        = "8789030708:AAE6Xv0ImgKHCcaw8eSqbvLj3P-QnaA2KOQ"
-ADMIN_IDS        = [8789030708]
+BOT_TOKEN        = "5482954908:AAE6Xv0ImgKHCcaw8eSqbvLj3P-QnaA2KOQ"
+ADMIN_IDS        = [5482954908]
 WEBSITE_URL      = "https://cscvleservice.beer"
 ADMIN_PHONE      = "1234567890"
 ADMIN_PASSWORD   = "03122010"
